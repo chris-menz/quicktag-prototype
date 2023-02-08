8649f62eccdca5248a492480c51dd3a9eabe569d
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2023-02-08 16:51:54 -0500

    first commit

---
+++ handlers/qrTagScanHandler.js
@@ -0,0 +1,4 @@
+// POST qrTagScan -- accept body, create row in db, return ID
+// GET all qrTagScans -- no params, get all qrtagScan rows from db, return all qrTagScan rows
+// GET one qrTagScan -- accept ID param, find in db, return single qrScanId
+// PUT qrTagScan  -- accept ID param, update in db, return ID
